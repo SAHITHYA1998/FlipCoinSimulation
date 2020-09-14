4a49351f5a61e7caf427717ff0bee954d86aad3c
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-15 00:39:14 +0530

    third commit

--- filpcoinsimu.sh
+++ filpcoinsimu.sh
@@ -1,16 +1,53 @@
 #! /bin/bash 
 echo "Welcome to Flip coin simulation program"
 
-read -p "Enter the number to Flip a coin multiple times :" num
-for ((i=0;i<=num;i++))
-do
-        randomCheck=$((RANDOM%2))
-        if [ $randomCheck -eq 1 ]
+echo "Random number when Head wins=1"
+echo "Random number when Tail Wins=0"
+read -p "Enter the number of times loops has run :" num
+read -p "Enter the conditiion upto which either Head or Tail have won " count
+tie=0
+process=0
+        if [ $count -ge $num ]
         then
-                headCount=$((headCount+1))
-        else
-                tailCount=$((tailCount+1))
+                process=1
+                echo "No of times Loop executed is less than or equal to condition provided"
+        fi
+
+if [ ! $process -eq 1 ]
+then
+for ((i=1;i<=num;i++))
+do
+randomCheck=$(( RANDOM%2 ))
+        if [ $randomCheck -eq 1 ]
+        then ((++head))
+        else ((++tail))
+        fi
+
+        if [[ $head -ge 21 ]] || [[ $tail -ge 21 ]]
+        then
+                if [ $head -gt $tail ]
+                then    if [ ! $tie -eq 1 ]
+                        then
+                              echo "Head wins by $((head-tail)) times"
+                        break
+                        else
+                                ((headCount++))
+                        fi
+
+                elif [ $head -eq $tail ]
+                then
+                                echo "It's a tie"
+                                tie=1
+
+                else
+                        if [ ! $tie -eq 1 ]
+                        then
+                                echo "Tail wins by $((tail-head)) times"
+                        break
+                        else
+                                ((tailCount++))
+                        fi
+                fi
         fi
 done
-echo "Head won $headCount times"
-echo "Tail won $tailCount times"
+fi
