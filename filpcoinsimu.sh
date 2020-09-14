10192987e894910e2f59ace0e2ddfb3f0a2f815b
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-15 00:35:59 +0530

    third commit

--- filpcoinsimu.sh
+++ filpcoinsimu.sh
@@ -1,9 +1,16 @@
 #! /bin/bash 
 echo "Welcome to Flip coin simulation program"
-randomCheck=$((RANDOM%2))
-if [ $randomCheck -eq 1 ]
-then
-        echo "Head won"
-else
-        echo "Tail won"
-fi
+
+read -p "Enter the number to Flip a coin multiple times :" num
+for ((i=0;i<=num;i++))
+do
+        randomCheck=$((RANDOM%2))
+        if [ $randomCheck -eq 1 ]
+        then
+                headCount=$((headCount+1))
+        else
+                tailCount=$((tailCount+1))
+        fi
+done
+echo "Head won $headCount times"
+echo "Tail won $tailCount times"
