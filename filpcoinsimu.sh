6e92865c2457827eb24755a3b1f682cdeb2572eb
SAHITHYA1998 <sahithyahi@gmail.com>
2020-09-15 00:30:34 +0530

    third commit

--- filpcoinsimu.sh
+++ filpcoinsimu.sh
@@ -1,1 +1,9 @@
-echo welcome to filpcoin simulation
+#! /bin/bash 
+echo "Welcome to Flip coin simulation program"
+randomCheck=$((RANDOM%2))
+if [ $randomCheck -eq 1 ]
+then
+        echo "Head won"
+else
+        echo "Tail won"
+fi
